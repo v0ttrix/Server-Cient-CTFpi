437b85124274654e398b6a822f176448c5502c3e
Ahmed Somaan <ahmedsomaan7@gmail.com>
2026-02-11 11:20:34 -0500

    created header component

--- Frontend/CTF/src/App.jsx
+++ Frontend/CTF/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Challenges from "./pages/Challenges";
+import Header from "./components/Header";
 import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="*" element={<Home />} />
           <Route path="/" element={<Home />} />
